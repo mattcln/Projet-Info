b5b49333f79d916ddb9698a8c7648abad26ff06c
mattisterik <mattisterik@users.noreply.github.com>
2018-11-22 12:37:35 +0100

    Projet Informatique (Lecuru - Nathan)

---
+++ Projet Info/Client.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_Info
+{
+    class Client
+    {
+        //  ATTRIBUTS
+        private string nom;
+        private string prénom;
+        private string typePermis;
+        private int ID;  // string ou int, à choisir en fonction du type d'ID
+        private List<Véhicule> listVeh;
+
+        //  CONSTRUCTEUR
+        public Client(string nom, string prénom, int ID)
+        {
+            this.nom = nom;
+            this.prénom = prénom;
+            this.ID = ID;
+            listVeh = new List<Véhicule>();
+        }
+
+        //  METHODE
+        public void AjoutVéhicule(Véhicule V)
+        {
+            listVeh.Add(V);
+        }
+        public void SuppVéhicule(Véhicule V)
+        {
+            listVeh.Remove(V);
+        }
+        public override string Tostring()
+        {
+            string str = "Véhicule: " + nom + "\n";
+            for (int i = 0; i < listVeh.Count; i++)
+            {
+                str += listVeh[i].ToString();
+            }
+            return str;
+        }
+    }
+}
---
+++ Projet Info/Véhicule.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_Info
+{
+    class Véhicule
+    {
+        //  ATTRIBUTS
+        protected string immat;
+        protected string typeVeh;
+        protected string marque;
+        protected string modèle;
+        protected string couleur;
+        protected int nbPortes;
+
+        //  CONSTRUCTEUR
+        public Véhicule(string immat, string typeVeh, string marque, string modèle, string couleur, int nbPortes)
+        {
+            this.immat = immat;
+            this.typeVeh = typeVeh;
+            this.marque = marque;
+            this.modèle = modèle;
+            this.couleur = couleur;
+            this.nbPortes = nbPortes;
+        }
+
+        // METHODE
+
+        
+
+    }
+}
